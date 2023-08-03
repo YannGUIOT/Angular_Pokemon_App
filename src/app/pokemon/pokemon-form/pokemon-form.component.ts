0ccc9dcf580274917bbcb39a689bed072a0cc7fc
YannGUIOT <guiot_yann@hotmail.fr>
2023-08-03 14:41:41 +0200

    Pokemon

--- src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -38,13 +38,8 @@ export class PokemonFormComponent implements OnInit {
   }
 
   onSubmit() {
-    
-    this.pokemonService.updatePokemon(this.pokemon).subscribe((pokemon) => {
-      if(pokemon) {
-        this.router.navigate(['/pokemon', pokemon.id])
-      }
-    });
-    
+    this.pokemonService.updatePokemon(this.pokemon)
+      .subscribe(() => this.router.navigate(['/pokemon', this.pokemon.id]));
   }
 
   isTypesValid(type: string): boolean {
