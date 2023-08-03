ea08907bc4710e99cc0fa70e29cf23c8e196808d
YannGUIOT <guiot_yann@hotmail.fr>
2023-08-03 11:29:38 +0200

    Pokemon

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Pokemon } from './pokemon';
 import { POKEMONS } from './mock-pokemon-list';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, of, tap } from 'rxjs';
 
 @Injectable()
 export class PokemonService {
 
-  getPokemonList(): Pokemon[] {
-    return POKEMONS;
+
+  constructor(private http: HttpClient) {}
+
+  // getPokemonList(): Pokemon[] {
+  //   return POKEMONS;
+  // }
+
+  getPokemonList(): Observable<Pokemon[]> {
+    return this.http.get<Pokemon[]>('api/pokemons').pipe(
+      tap((response) => console.table(response)),
+      catchError((error) => {
+        console.log(error);
+        return of([]);
+      })
+    );
   }
 
   getPokemonById(pokemonId: number): Pokemon|undefined {
