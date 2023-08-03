7f20194b7d58c1f45ed527bb71b23404944f5809
YannGUIOT <guiot_yann@hotmail.fr>
2023-08-03 16:54:22 +0200

    Pokemon

--- src/app/pokemon/search-pokemon/search-pokemon.component.ts
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
 import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-search-pokemon',
   templateUrl: './search-pokemon.component.html',
   styleUrls: ['./search-pokemon.component.css']
 })
-export class SearchPokemonComponent {
+export class SearchPokemonComponent implements OnInit {
 
   searchTerms = new Subject<string>();
   pokemons$: Observable<Pokemon[]>;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private pokemonService: PokemonService
+    ) {}
+
+  ngOnInit(): void {
+    this.pokemons$ = this.searchTerms.pipe(
+      debounceTime(300), // élimine les requêtes dont l'ajout de nouvelle lettre est inférieur à 300ms
+      distinctUntilChanged(), // élimine les requêtes doublons
+      switchMap((term) => this.pokemonService.searchPokemonList(term))
+    );
+  }
 
   search(term: string) {
     this.searchTerms.next(term);
