239443010c98e4992a052b5e794c5647cc9d0012
YannGUIOT <guiot_yann@hotmail.fr>
2023-08-03 16:32:04 +0200

    Pokemon

--- src/app/pokemon/search-pokemon/search-pokemon.component.ts
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-search-pokemon',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-pokemon.component.css']
 })
 export class SearchPokemonComponent {
+
+  searchTerms = new Subject<string>();
+  pokemons$: Observable<Pokemon[]>;
+
+  constructor(private router: Router) {}
+
+  search(term: string) {
+
+  }
+
+  goToDetail(pokemon: Pokemon) {
+    const link = ['/pokemon', pokemon.id];
+    this.router.navigate(link);
+  }
 
 }
