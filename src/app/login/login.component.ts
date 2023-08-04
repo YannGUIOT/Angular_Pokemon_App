a2fd370e7c65b498db6b71c056f4be0a7909565b
YannGUIOT <guiot_yann@hotmail.fr>
2023-08-04 18:58:23 +0200

    Pokemon

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,7 +4,8 @@ import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
   message: string = 'Vous êtes déconnecté. (pikachu/pikachu)';
