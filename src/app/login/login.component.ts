12782a532ffa4fa9341585ad6d94608941e22e72
YannGUIOT <guiot_yann@hotmail.fr>
2023-08-04 17:24:03 +0200

    Pokemon

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  message: string = 'Vous êtes déconnecté. (pikachu/pikachu)';
+  name: string;
+  password: string;
+  auth: AuthService;
 
+  constructor(
+    private authService: AuthService, 
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.auth = this.authService
+  }
+
+  // Informe l'utilisateur sur son authentfication.
+  setMessage() {
+    this.message = this.auth.isLoggedIn ?
+      'Vous êtes connecté.' : 'Identifiant ou mot de passe incorrect.';
+  }
+
+  // Connecte l'utilisateur auprès du Guard
+  login() {
+      this.message = 'Tentative de connexion en cours ...';
+      this.auth.login(this.name, this.password)
+        .subscribe((isLoggedIn: boolean) => {
+          this.setMessage();
+          if (isLoggedIn) {
+            this.router.navigate(['/pokemons']);
+          } else {
+            this.password = '';
+            this.router.navigate(['/login']);
+          }
+      });
+  }
+
+  // Déconnecte l'utilisateur
+  logout() {
+      this.auth.logout();
+      this.setMessage();
+  }
 }
