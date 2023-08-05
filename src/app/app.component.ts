2565d84b159a0df335b69568dc29470834c6cee8
YannGUIOT <guiot_yann@hotmail.fr>
2023-08-05 18:43:54 +0200

    Pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,11 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',  // donne un nom au composant pour l'identifier par la suite par la balise <app-root>
-  templateUrl: './app.component.html', // cela pourrait plus être template: `<h1>Welcome to {{title}} !</h1>`
+  selector: 'app-root',  // will then be executed by <app-root></app-root>
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {  
-   // il est importé dans le fichier app.module.ts
-   // => import { AppComponent } from './app.component';
 
    constructor(
     private router: Router
